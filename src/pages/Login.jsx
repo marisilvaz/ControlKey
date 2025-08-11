88a82358e007b8dbca423908e66dd88233042105
Vivian Ribeiro <ribeirovivian2015@gmail.com>
2025-08-11 20:02:20 -0300

    colocando porta do backend no login e instalando o axios

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Login.css';
+import api from '../services/api';
 // import logo from '../assets/LOGOCERTO.png';
 
 function Login() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleLogin = async () => {
+        try {
+            const res = await api.post('/login', {
+                username,
+                password
+            });
+            console.log('Resposta do backend:', res.data);
+            // Aqui você pode redirecionar ou salvar token, etc.
+        } catch (err) {
+            console.error('Erro no login:', err);
+        }
+    };
+
     return (
         <div className="login-container">
             <header className="login-header">
@@ -14,12 +31,22 @@ function Login() {
                 <h2>CONTROLKEY</h2>
 
                 <div className='input-group'>
-                    <input type='text' placeholder='Username' />
+                    <input
+                        type='text'
+                        placeholder='Username'
+                        value={username}
+                        onChange={e => setUsername(e.target.value)}
+                    />
                 </div>
                 <div className='input-group'>
-                    <input type='password' placeholder='Password' />
+                    <input
+                        type='password'
+                        placeholder='Password'
+                        value={password}
+                        onChange={e => setPassword(e.target.value)}
+                    />
                 </div>
-                <button className='btn-login'>Entrar</button>
+                <button className='btn-login' onClick={handleLogin}>Entrar</button>
                 <a href="/" className='esqueceu-senha'>Esqueceu a senha?</a>
             </div>
         </div>
